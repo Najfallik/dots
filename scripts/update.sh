931015c996fa795ecb102a32838c606dd8145b4f
Najfallik <simonolej@gmail.com>
2025-02-11 15:17:46 +0100

    end of jedi war

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,32 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-# ASCII ART
-echo ""
-echo "███████╗██╗██╗  ██╗██╗  ██╗ ██████╗ ███████╗    ██╗   ██╗███████╗"
-echo "██╔════╝██║██║ ██╔╝██║ ██╔╝██╔═══██╗██╔════╝    ██║   ██║██╔════╝"
-echo "███████╗██║█████╔╝ █████╔╝ ██║   ██║███████╗    ██║   ██║███████╗"
-echo "╚════██║██║██╔═██╗ ██╔═██╗ ██║   ██║╚════██║    ██║   ██║╚════██║"
-echo "███████║██║██║  ██╗██║  ██╗╚██████╔╝███████║    ╚██████╔╝███████║"
-echo "╚══════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝ ╚══════╝"
-echo ""
+## Start echo
+echo "Starting sikko's Fedora update script..."
 
-# Read sudo password
-printf "Enter your sudo password: "
-read -r -s SUDO_PASSWORD
+## Check for updates
+echo "Checking for updates..."
+UPDATES=$(sudo dnf check-update 2>&1)
 
-# Check for updates
-UPDATES=$(echo "$SUDO_PASSWORD" | sudo -s dnf check-update)
-
-# If updates available, install them
-if [ -n "$UPDATES" ]; then
+## If updates available, install them
+if [[ $UPDATES != "" ]]; then
   echo "Updates are available. Installing..."
-  echo "$SUDO_PASSWORD" | sudo -s dnf upgrade -y
+  sudo dnf upgrade -y
 else
   echo "No updates available"
 fi
 
-# Clean caches
-echo "$SUDO_PASSWORD" | sudo dnf clean all
-
-# Clear sudo password from memory
-SUDO_PASSWORD=""
+## Clean caches
+echo "Cleaning up..."
+sudo dnf clean all
