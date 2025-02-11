7ecd0e30790d0c5fe6775b2e77fe9597fb95235f
Najfallik <simonolej@gmail.com>
2025-02-11 14:30:32 +0100

    legendary battle incoming

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,32 @@
+#!/bin/sh
+
+# ASCII ART
 echo ""
-echo "███████╗██╗██╗  ██╗██╗  ██╗ ██████╗ ███████╗    ██╗   ██╗███████╗";
-echo "██╔════╝██║██║ ██╔╝██║ ██╔╝██╔═══██╗██╔════╝    ██║   ██║██╔════╝";
-echo "███████╗██║█████╔╝ █████╔╝ ██║   ██║███████╗    ██║   ██║███████╗";
-echo "╚════██║██║██╔═██╗ ██╔═██╗ ██║   ██║╚════██║    ██║   ██║╚════██║";
-echo "███████║██║██║  ██╗██║  ██╗╚██████╔╝███████║    ╚██████╔╝███████║";
-echo "╚══════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝ ╚══════╝";
+echo "███████╗██╗██╗  ██╗██╗  ██╗ ██████╗ ███████╗    ██╗   ██╗███████╗"
+echo "██╔════╝██║██║ ██╔╝██║ ██╔╝██╔═══██╗██╔════╝    ██║   ██║██╔════╝"
+echo "███████╗██║█████╔╝ █████╔╝ ██║   ██║███████╗    ██║   ██║███████╗"
+echo "╚════██║██║██╔═██╗ ██╔═██╗ ██║   ██║╚════██║    ██║   ██║╚════██║"
+echo "███████║██║██║  ██╗██║  ██╗╚██████╔╝███████║    ╚██████╔╝███████║"
+echo "╚══════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝ ╚══════╝"
 echo ""
-echo "Updating system..."
-sudo dnf update -y
-sudo dnf upgrade -y --refresh
-echo "Cleaning up..."
-sudo dnf clean all
-echo "All done!"
+
+# Read sudo password
+printf "Enter your sudo password: "
+read -r -s SUDO_PASSWORD
+
+# Check for updates
+UPDATES=$(echo "$SUDO_PASSWORD" | sudo -s dnf check-update)
+
+# If updates available, install them
+if [ -n "$UPDATES" ]; then
+  echo "Updates are available. Installing..."
+  echo "$SUDO_PASSWORD" | sudo -s dnf upgrade -y
+else
+  echo "No updates available"
+fi
+
+# Clean caches
+echo "$SUDO_PASSWORD" | sudo dnf clean all
+
+# Clear sudo password from memory
+SUDO_PASSWORD=""
